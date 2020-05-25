784db2854a39f8b7c263a3e8121b8d450b7a768d
Alisi Daniel <alisidaniel@gmail.com>
2020-05-25 17:00:30 +0100

    navigation

---
+++ screens/profile.js
@@ -0,0 +1,126 @@
+// import React, {Component} from 'react';
+// import {Alert, StyleSheet, Text, TouchableHighlight, View, TouchableOpacity} from 'react-native';
+// import {LoginButton, ShareDialog} from 'react-native-fbsdk';
+// import ImagePicker from 'react-native-image-crop-picker';
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from '@react-native-community/google-signin';
+
+
+// const SHARE_LINK_CONTENT = {
+//   contentType: 'link',
+//   contentUrl: 'https://www.facebook.com/',
+// };
+
+// export default class App extends Component<{}> {
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       images: [],
+//       userInfo: {}
+//     };
+//   }
+//   componentDidMount(){
+//     GoogleSignin.configure({
+//       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+//       webClientId: '472745786335-tp6kri3t681bf9a62rfe46cs7nei0l3h.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+//       //hostedDomain: '', // specifies a hosted domain restriction
+//       //loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+//       forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+//       //accountName: '', // [Android] specifies an account name on the device that should be used
+//       //iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+//     })
+//   }
+//   pickMultiple() {
+//     ImagePicker.openPicker({
+//       multiple: true
+//     }).then(images => {
+//       console.log(images);
+//     }).catch(e => alert(e));
+//   }
+
+//   _shareLinkWithShareDialog = async () => {
+//     const canShow = await ShareDialog.canShow(SHARE_LINK_CONTENT);
+//     if (canShow) {
+//       try {
+//         const {isCancelled, postId} = await ShareDialog.show(
+//           SHARE_LINK_CONTENT,
+//         );
+//         if (isCancelled) {
+//           Alert.alert('Share cancelled');
+//         } else {
+//           Alert.alert('Share success with postId: ' + postId);
+//         }
+//       } catch (error) {
+//         Alert.alert('Share fail with error: ' + error);
+//       }
+//     }
+//   };
+
+//   _signIn = async () => {
+//     try {
+//       await GoogleSignin.hasPlayServices();
+//       const userInfo = await GoogleSignin.signIn();
+//       console.log(userInfo)
+//       // this.setState({ userInfo });
+//     } catch (error) {
+//       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+//         // user cancelled the login flow
+//       } else if (error.code === statusCodes.IN_PROGRESS) {
+//         // operation (e.g. sign in) is in progress already
+//       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+//         // play services not available or outdated
+//       } else {
+//         // some other error happened
+//       }
+//       console.log(error)
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+
+//         <TouchableOpacity onPress={this.pickMultiple.bind(this)} style={{backgroundColor:'green',height:50,width:100,alignContent:'center'}}>
+//           <Text style={{paddingTop:14}}>Choose photo</Text>
+//         </TouchableOpacity>
+        
+//         <LoginButton
+//           onLoginFinished={(error, data) => {
+//             console.log('AAAA');
+//             setTimeout(() => {
+//               Alert.alert(JSON.stringify(error || data, null, 2));
+//             }, 1000);
+//           }}
+//         />
+
+//       <GoogleSigninButton
+//         style={{ width: 192, height: 48 }}
+//         size={GoogleSigninButton.Size.Wide}
+//         color={GoogleSigninButton.Color.Dark}
+//         onPress={this._signIn} />
+
+//         <TouchableHighlight onPress={this._shareLinkWithShareDialog}>
+//           <Text style={styles.shareText}>Share link with ShareDialog</Text>
+//         </TouchableHighlight>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   shareText: {
+//     fontSize: 20,
+//     margin: 10,
+//   },
+// });
