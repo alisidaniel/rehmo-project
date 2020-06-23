06116dd8af2654082a1141a9e5a0f22719367a60
Alisi Daniel <alisidaniel@gmail.com>
2020-06-23 07:15:38 +0100

    rabbitmqTest

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-module.export = {
-port: process.env.PORT,
-
-  // Mongo connect
-  mongo_db: process.env.MONGO_USER,
-  mongo_user: process.env.MONGO_USER,
-  mongo_password: process.env.MONGO_PASSWORD,
-};
